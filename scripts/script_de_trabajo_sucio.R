00e98fcc397eebb166275a82095b9f4f9e9e9c3c
Armando Ocampo <fooseaov@gmail.com>
2023-02-16 17:24:05 -0600

    generando dataset de trabajo

--- scripts/script_de_trabajo_sucio.R
+++ scripts/script_de_trabajo_sucio.R
@@ -1,4 +1,30 @@
-#### llamando dataset
+#### prueba de carga
 
 raw_data <- read_csv('raw_data/risk_factors_cervical_cancer.csv')
+
+is.na(raw_data) <- df == "?"
+raw_data[raw_data == '?'] <- NA
+
+write.csv(raw_data, 'clean_data/risk_factors_cervical_cancer_raw.csv',
+          row.names = FALSE)
+
+prueba <- read.csv('clean_data/risk_factors_cervical_cancer_raw.csv')
+
+
+
+### iniciando proyecto
+
+# cargando dataset crudo
+
+raw_data <- read_csv('raw_data/risk_factors_cervical_cancer_raw.csv')
+glimpse(raw_data)
+
+# seleccionando variables de interés
+
+cancer_data <- raw_data %>%
+  select(age, number_of_sexual_partners, first_sexual_intercorse,
+         smokes, smokes_years, smokes_packs_year, hormonal_anticonceptives,
+         dx_cancer)
+
+# realizando grafico de correlacion
 
